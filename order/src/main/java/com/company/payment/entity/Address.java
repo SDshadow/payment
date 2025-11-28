3758d4f0cdaae57afa09f69624d55dc987a24c17
SDshadow <2727192251@qq.com>
2025-11-29 01:06:53 +0800

    Address

--- order/src/main/java/com/company/payment/entity/Address.java
+++ order/src/main/java/com/company/payment/entity/Address.java
@@ -1,5 +1,30 @@
 package com.company.payment.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
 //地址值对象
+@Data
+@Embeddable
 public class Address {
+    //收货人姓名
+    @Column(nullable = false, length = 50)
+    private String receiverName;
+    //收货人电话
+    @Column(nullable = false, length = 15)
+    private String receiverPhone;
+    //省市区三级地址
+    @Column(nullable = false, length = 100)
+    private String province;
+    //省市区三级地址
+    @Column(nullable = false, length = 100)
+    private String city;
+    //省市区三级地址
+    @Column(nullable = false, length = 100)
+    private String district;
+    //详细地址
+    @Column(nullable = false, length = 200)
+    private String detailAddress;
+
 }
